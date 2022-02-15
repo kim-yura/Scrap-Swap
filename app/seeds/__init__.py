7b0a1c57d151b1f98cb801e261566652ad4b1881
Yu Ra Kim <yurakim.designs@gmail.com>
2022-02-15 10:01:31 -0500

    Seed: swap_targets

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .yarn_weights import seed_yarn_weights, undo_yarn_weights
+from .swap_targets import seed_swap_targets, undo_swap_targets
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -12,6 +13,7 @@ seed_commands = AppGroup('seed')
 def seed():
     seed_users()
     seed_yarn_weights()
+    seed_swap_targets()
 
 
 # Creates the `flask seed undo` command
@@ -19,3 +21,4 @@ def seed():
 def undo():
     undo_users()
     undo_yarn_weights()
+    undo_swap_targets()
