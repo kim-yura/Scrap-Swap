53112b57bfdf0f4253cd9704821920d278ff61e0
Yu Ra Kim <yurakim.designs@gmail.com>
2022-02-15 15:42:07 -0500

    Scraps POST route and store

--- react-app/src/store/scraps.js
+++ react-app/src/store/scraps.js
@@ -1,7 +1,7 @@
 import { csrfFetch } from '../helpers';
 
 const LOAD_ALL_SCRAPS = 'scraps/loadAllScraps';
-// const CREATE_SCRAP = 'scraps/CREATE_SCRAP';
+const CREATE_SCRAP = 'scraps/createScrap';
 // const EDIT_SCRAP = 'scraps/EDIT_SCRAP';
 // const DELETE_SCRAP = 'scraps/DELETE_SCRAP';
 
@@ -20,12 +20,52 @@ const loadAllScrapsAction = (allScraps) => ({
 });
 
 
+// -------------------- CREATE -------------------- //
+
+export const createScrap = ({
+    userId,
+    title,
+    imageURL,
+    yarnWeightId,
+    fiberContent,
+    yardage,
+    allergens,
+    textContent,
+    swapTargetId,
+    user
+}) => async (dispatch) => {
+    const response = await csrfFetch('/api/scraps/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            user_id: userId,
+            title,
+            image_url: imageURL,
+            yarn_weight_id: yarnWeightId,
+            fiber_content: fiberContent,
+            yardage,
+            allergens,
+            text_content: textContent,
+            swap_target_id: swapTargetId,
+            user
+        })
+    });
+
+    if (response.ok) {
+        const scrap = await response.json();
+        dispatch(createScrapAction(scrap));
+        return scrap;
+    }
+};
+
+const createScrapAction = (scrap) => ({
+    type: CREATE_SCRAP,
+    scrap
+});
 
 
-// const createScrap = (scrap) => ({
-//     type: CREATE_SCRAP,
-//     scrap
-// });
 
 // const editScrap = (scrap) => ({
 //     type: EDIT_SCRAP,
@@ -49,6 +89,7 @@ const scrapReducer = (state = {}, action) => {
                 newState[scrap.id] = scrap;
             });
             return newState;
+        case CREATE_SCRAP:
 
         default:
             return state;
