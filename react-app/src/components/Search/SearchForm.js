c877dc63d07da57c7492149332dea935bf95d00f
Yu Ra Kim <yurakim.designs@gmail.com>
2022-02-18 17:47:46 -0500

    SearchForm complete

--- react-app/src/components/Search/SearchForm.js
+++ react-app/src/components/Search/SearchForm.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import ScrapCard from '../ScrapCard/ScrapCard';
 
 import { loadAllScraps } from '../../store/scraps';
 
@@ -24,6 +25,8 @@ const SearchForm = () => {
     const [noDogs, setNoDogs] = useState(false);
     const [otherAllergens, setOtherAllergens] = useState('');
 
+    const [searchResultsArr, setSearchResultsArr] = useState([]);
+
     const toggleCats = () => {
         setNoCats(!noCats);
     };
@@ -32,6 +35,7 @@ const SearchForm = () => {
     };
 
     const handleSearch = () => {
+        setSearchResultsArr([]);
         let searchResultsArr = [];
         Object.values(allScraps).forEach(scrap => {
             if (scrap.title.includes(searchParams) || scrap.textContent.includes(searchParams)) {
@@ -42,7 +46,7 @@ const SearchForm = () => {
         });
 
         // Yarn weight
-        if (yarnWeightId) {
+        if (parseInt(yarnWeightId) !== 0) {
             let copyArr = searchResultsArr.slice();
             searchResultsArr = [];
             copyArr.forEach(scrap => {
@@ -53,7 +57,7 @@ const SearchForm = () => {
         };
 
         // Swap id
-        if (swapTargetId) {
+        if (parseInt(swapTargetId) !== 0) {
             let copyArr = searchResultsArr.slice();
             searchResultsArr = [];
             copyArr.forEach(scrap => {
@@ -62,7 +66,6 @@ const SearchForm = () => {
                 };
             });
         };
-        console.log(searchResultsArr);
 
         // Allergens
         if (noCats) {
@@ -92,75 +95,87 @@ const SearchForm = () => {
                 };
             });
         };
-        console.log(searchResultsArr);
+        setSearchResultsArr(searchResultsArr);
+        window.scrollTo(0, 0);
     };
 
     return (
-        <div className='search-form-container'>
-            <div id='search-form-header'>
-                Search for...
-            </div>
-            <input
-                onChange={(e) => setSearchParams(e.target.value)}
-                value={searchParams}
-                id='search-params'
-                type='text'
-                placeholder='Search for anything'
-            />
-            <select defaultValue={0} onChange={(e) => setYarnWeightId(e.target.value)}>
-                <option value={0}>Choose a yarn weight</option>
-                <option value={1} required>Thread</option>
-                <option value={2} required>Lace</option>
-                <option value={3} required>Light Fingering</option>
-                <option value={4} required>Fingering</option>
-                <option value={5} required>Sport</option>
-                <option value={6} required>DK</option>
-                <option value={7} required>Worsted</option>
-                <option value={8} required>Aran</option>
-                <option value={9} required>Bulky</option>
-                <option value={10} required>Super Bulky</option>
-                <option value={11} required>Jumbo</option>
-            </select>
-            <p className='label'>
-                I need at least this many yards...
-            </p>
-            <input
-                onChange={(e) => setMinimumYardage(e.target.value)}
-                value={minimumYardage}
-                id='minimum-yardage'
-                type='number'
-            />
-            <p className='label'>
-                I'm looking to...
-            </p>
-            <select defaultValue={0} onChange={(e) => setSwapTargetId(e.target.value)}>
-                <option value={0}>What are you swapping for?</option>
-                <option value={1} required>Trade</option>
-                <option value={2} required>Pay for postage</option>
-                <option value={3} required>Receive for free!</option>
-            </select>
-            <p className='label'>
-                I'm allergic to...
-            </p>
-            <div className='allergen-buttons'>
+        <div className='search'>
+            <div className='search-form-container'>
+                <div id='search-form-header'>
+                    Search for...
+                </div>
+                <input
+                    onChange={(e) => setSearchParams(e.target.value)}
+                    value={searchParams}
+                    id='search-params'
+                    type='text'
+                    placeholder='Search for anything'
+                />
+                <select defaultValue={0} onChange={(e) => setYarnWeightId(e.target.value)}>
+                    <option value={0}>Choose a yarn weight</option>
+                    <option value={1} required>Thread</option>
+                    <option value={2} required>Lace</option>
+                    <option value={3} required>Light Fingering</option>
+                    <option value={4} required>Fingering</option>
+                    <option value={5} required>Sport</option>
+                    <option value={6} required>DK</option>
+                    <option value={7} required>Worsted</option>
+                    <option value={8} required>Aran</option>
+                    <option value={9} required>Bulky</option>
+                    <option value={10} required>Super Bulky</option>
+                    <option value={11} required>Jumbo</option>
+                </select>
+                <p className='label'>
+                    I need at least this many yards...
+                </p>
+                <input
+                    onChange={(e) => setMinimumYardage(e.target.value)}
+                    value={minimumYardage}
+                    id='minimum-yardage'
+                    type='number'
+                />
+                <p className='label'>
+                    I'm looking to...
+                </p>
+                <select defaultValue={0} onChange={(e) => setSwapTargetId(e.target.value)}>
+                    <option value={0}>What are you swapping for?</option>
+                    <option value={1} required>Trade</option>
+                    <option value={2} required>Pay for postage</option>
+                    <option value={3} required>Receive for free!</option>
+                </select>
+                <p className='label'>
+                    I'm allergic to...
+                </p>
+                <div className='allergen-buttons'>
+                    <button
+                        className={noCats ? 'toggle-yes' : ''}
+                        onClick={toggleCats}>Cats</button>
+                    <button
+                        className={noDogs ? 'toggle-yes' : ''}
+                        onClick={toggleDogs}>Dogs</button>
+                </div>
+                <input
+                    onChange={(e) => setOtherAllergens(e.target.value)}
+                    value={otherAllergens}
+                    type='text'
+                    placeholder='...others'
+                />
                 <button
-                    className={noCats ? 'toggle-yes' : ''}
-                    onClick={toggleCats}>Cats</button>
-                <button
-                    className={noDogs ? 'toggle-yes' : ''}
-                    onClick={toggleDogs}>Dogs</button>
+                    onClick={handleSearch}
+                    className='submit-search-button'>
+                    Gimme those Scraps!
+                </button>
             </div>
-            <input
-                onChange={(e) => setOtherAllergens(e.target.value)}
-                value={otherAllergens}
-                type='text'
-                placeholder='...others'
-            />
-            <button
-                onClick={handleSearch}
-                className='submit-search-button'>
-                Gimme those Scraps!
-            </button>
+            <div className='search-page'>
+                <div className='search-results-gradient' />
+                <div className='search-container'>
+                    {searchResultsArr.length > 0 &&
+                        searchResultsArr.map((scrap, idx) =>
+                            <ScrapCard scrap={scrap} key={idx} />
+                        )}
+                </div>
+            </div>
         </div>
     )
 };
